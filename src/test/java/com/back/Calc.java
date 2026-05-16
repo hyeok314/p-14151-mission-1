b78ebec9e8b5c1e8f5a791582ddb640afc04ae3d
hyeok314 <joysys8272@gmail.com>
2026-05-17 00:38:14 +0900

    t3 blue

--- src/test/java/com/back/Calc.java
+++ src/test/java/com/back/Calc.java
@@ -2,8 +2,10 @@ package com.back;
 
 public class Calc {
     public static int run(String expression) {
-        if(expression.equals("2 + 1")) return 3;
-        if(expression.equals("2 + 2")) return 4;
-        return 2;
+        String[] expBits = expression.split(" \\+ ");
+
+        int num1 = Integer.parseInt(expBits[0]);
+        int num2 = Integer.parseInt(expBits[1]);
+        return num1 + num2;
     }
 }
