ccd3ce8a0dcbfd9066bdac0e0aa994c399580e9b
hyeok314 <joysys8272@gmail.com>
2026-05-17 23:46:39 +0900

    t6 blue & t7 green

--- src/test/java/com/back/Calc.java
+++ src/test/java/com/back/Calc.java
@@ -2,9 +2,17 @@ package com.back;
 
 public class Calc {
     public static int run(String expression) {
-        if(expression.equals("2 - 1")) return 1;
-        if(expression.equals("3 - 1")) return 2;
+        if(expression.contains("-")) {
+            String[] expBits = expression.split(" \\- ");
+
+            int num1 =  Integer.parseInt(expBits[0]);
+            int num2 = Integer.parseInt(expBits[1]);
+
+            return num1 - num2;
+        }
+
         String[] expBits = expression.split(" \\+ ");
+
 
         int num1 = Integer.parseInt(expBits[0]);
         int num2 = Integer.parseInt(expBits[1]);
